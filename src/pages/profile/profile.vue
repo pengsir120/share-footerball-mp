50478cf7f993d08574e9aba1e6ec5029d88bd7cb
chenruipeng <1259903760@qq.com>
2024-11-10 21:16:13 +0800

    feat: 客服中心、问题详情、个人中心页面

---
+++ src/pages/profile/profile.vue
@@ -0,0 +1,63 @@
+<template>
+	<view class="profile-box">
+		<view class="profile-top">
+			<view v-if="isLogin" class="profile-user">
+				<image class="user-avatar" src="../../static/avatar.png" mode="widthFix"></image>
+				<view class="user-nickname ellipsis">哈喽伍德哈喽伍德哈喽伍德哈喽伍德哈喽伍德哈喽伍德</view>
+			</view>
+			<button v-else class="button" @tap="handleTap">点击登录</button>
+		</view>
+	</view>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const isLogin = ref(true);
+
+const handleTap = () => {
+	isLogin.value = true;
+};
+</script>
+
+<style>
+page {
+	background-color: #f1f1f1;
+}
+
+.profile-box {
+	width: 90%;
+	margin: 0 auto;
+}
+
+.profile-top {
+	height: 200rpx;
+	background-color: #ffffff;
+	display: flex;
+	align-items: center;
+	border-radius: 10rpx;
+	margin: 20rpx 0;
+}
+
+.profile-user {
+	display: flex;
+	align-items: center;
+	flex: 1;
+}
+
+.user-avatar {
+	width: 120rpx;
+	height: 120rpx;
+	margin: 0 40rpx;
+	border-radius: 50%;
+	box-shadow: 0 0 20rpx #d1d1d1;
+}
+
+.user-nickname {
+	font-size: 50rpx;
+	font-weight: 700;
+	margin-right: 40rpx;
+	flex: 1;
+	width: 435rpx;
+}
+</style>
---
+++ src/pages/question/question.vue
@@ -0,0 +1,47 @@
+<template>
+	<view class="question-box">
+		<view v-for="(item, index) in list" :key="item.id">
+			<view class="question-title">{{ index + 1 }}. {{ item.title }}</view>
+			<view class="question-desc">
+				{{ item.desc }}
+			</view>
+		</view>
+	</view>
+</template>
+
+<script setup>
+import { onLoad } from '@dcloudio/uni-app';
+import { ref } from 'vue';
+import { getContactQuestionByType } from '../../pages-data/index.js';
+
+const list = ref([]);
+onLoad((options) => {
+	if (options.type) {
+		list.value = getContactQuestionByType(+options.type);
+	}
+});
+</script>
+
+<style>
+page {
+	background-color: #f1f1f1;
+}
+
+.question-box {
+	width: 80%;
+	margin: 0 auto;
+	background-color: #ffffff;
+	padding: 20rpx 5%;
+}
+
+.question-title {
+	height: 100rpx;
+	line-height: 100rpx;
+	font-weight: 700;
+	font-size: 40rpx;
+}
+
+.question-desc {
+	font-size: 30rpx;
+}
+</style>
